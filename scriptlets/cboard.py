4dbe6f7be640c0baf9c1235ccb6d5a6a168c52cc
Dan <borgdog@gmail.com>
2021-09-03 20:22:19 -0700

    just some comment updates

--- scriptlets/cboard.py
+++ scriptlets/cboard.py
@@ -9,12 +9,12 @@ class Cboard(Scriptlet):
 
     def lightemup(self, value, prev_value, player_num, change, **kwargs):
         self.log.info('value '+str(value)+' change '+str(change))
-        if value > 120:
-            self.delay.add(name='blank_lights', ms=3000, callback=self.blankem) #blank after 3 seconds
+        if value > 120:                                                             #game won at 121 points
+            self.delay.add(name='blank_lights', ms=3000, callback=self.blankem)     #blank all after 3 seconds
             for x in range(1, 121):
                 self.machine.shows['score_flash'].play(show_tokens=dict(led='l_p'+str(player_num)+'_'+str(x)), loops=5)  # flash all for 3 seconds 
-        else:
-            for x in range(value-change+1, value+1):
+        else:                                                                       #not at end of game
+            for x in range(value-change+1, value+1):                                # flash the amount scored
                 self.machine.shows['score_flash'].play(show_tokens=dict(led='l_p'+str(player_num)+'_'+str(x)), loops=5)  # flash change for 3 seconds
                 lname = 'l_p'+str(player_num)+'_'+str(x)
                 self.log.info('Setting rgb '+lname + ' on')
